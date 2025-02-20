fc433615f9b13485f5a8f3d2a28d1788a05d3d4f
mushi1155 <musharafaif155@gmail.com>
2025-02-20 13:01:45 +0500

    added new features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 
 import './App.css';
 import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
 
 function App() {
   return (
     <>
       
-<Navbar title="Textutils"/>
+    <Navbar title="Textutils"/>
+    <div className="container" my-3 >
+      <TextForm heading= "Enter the Text to analyse"/>
+
+    </div>
+    
+    
     </>
   );
 }
