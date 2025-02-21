7aec08c8dbd04cded833122e08806a8bb6316989
mushi1155 <musharafaif155@gmail.com>
2025-02-21 14:47:01 +0500

    dark mode enable in navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 
+// import { useState } from 'react';
 import './App.css';
 import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
+import React, {useState} from 'react';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const toggleMode = ()=>{
+    if (mode === 'light'){
+      setMode('dark')
+    }
+    else{
+      setMode('light')
+    }
+  }
   return (
     <>
       
-    <Navbar title="Textutils"/>
+    <Navbar title="Textutils" mode={mode} toggleMode={toggleMode}/>
     <div className="container" my-3 >
       <TextForm heading= "Enter the Text to analyse"/>
 
