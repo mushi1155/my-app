40b27088623ee07d9d54c2eff172e8f856eac411
mushi1155 <musharafaif155@gmail.com>
2025-02-21 16:58:52 +0500

    added alert function

--- src/App.js
+++ src/App.js
@@ -2,26 +2,40 @@
 // import { useState } from 'react';
 import './App.css';
 import About from './Components/About';
+import Alert from './Components/Alert';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 import React, {useState} from 'react';
 
 function App() {
   const [mode, setMode] = useState('light');
+  const [alert , setAlert]=useState("null");
+  const showAlert= (message , type)=>{
+    setAlert({
+      msg: message,
+      type:type,
+    })
+
+  }
   const toggleMode = ()=>{
     if (mode === 'light'){
-      setMode('dark')
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success")
     }
     else{
-      setMode('light')
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success")
     }
   }
   return (
     <>
       
     <Navbar title="Textutils" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
     <div className="container" my-3 >
-      <TextForm heading= "Enter the Text to analyse"/>
+      <TextForm heading= "Enter the Text to analyse" mode={mode}/>
 
     </div>
     <About/>
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -20,22 +20,22 @@ export default function TextForm(props) {
     
     return (
         <>
-        <div>
+        <div className='container' style={{color: props.mode==='dark'?'white':'#042743'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox"  rows="8"></textarea>
+            <textarea className="form-control" value={text} style={{backgroundColor: props.mode==='dark'?'grey':'white',color: props.mode==='dark'?'white':'#042743'}} onChange={handleOnChange} id="myBox"  rows="8"></textarea>
             </div>
             <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to upper case</button>
             <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lower case</button>
 
       
         </div>
-        <div className='container' my-3>
+        <div className='container' my-3 style={{color: props.mode==='dark'?'white':'#042743'}}>
             <h1>your text summary</h1>
             <p> {text.split(" ").length} words and {text.length} characters</p>
             <p> It will take { 0.008 * text.split(" ").length} minutes to read this paragraph</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length>0 ? text: "Enter something in the text book to preview it here"}</p>
 
         </div>
         </>
